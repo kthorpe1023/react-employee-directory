da30aa698a33991153b0df4ce3ac706f6640ce08
Katie Thorpe <kthorpe1023@gmail.com>
2020-08-11 08:39:37 -0600

    employees.json file created w seed data

--- employee-tracker/src/App.js
+++ employee-tracker/src/App.js
@@ -1,8 +1,16 @@
 import React from "react";
 import EmployeeList from "./components/EmployeeList"
+import employees from "./employees.json"
 
 function App(){
-    return <EmployeeList />
-}
+    return (
+    <EmployeeList 
+    name={employees[0].name}
+    image={employees[0].image}
+    occupation={employees[0].occupation}
+    location={employees[0].location}
+    />
+    )
+};
 
 export default App;
